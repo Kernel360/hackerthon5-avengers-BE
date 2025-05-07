2f0f44d406a03fca1ce74f530bfb8739486acb24
banxzxx <jwbang1643@gmail.com>
2025-05-07 11:14:34 +0900

    0507 init

---
+++ src/main/java/com/hackerthon5/avengers_BE/AvengersBeApplication.java
@@ -0,0 +1,13 @@
+package com.hackerthon5.avengers_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersBeApplication.class, args);
+	}
+
+}
