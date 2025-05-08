deb8cc7826b7e38d1994c0cff75af50f9955f587
YunNote <zzdd1558@gmail.com>
2025-05-09 01:08:18 +0900

    feature: sample

--- src/main/java/com/hackerthon5/avengers_BE/member/service/MemberService.java
+++ src/main/java/com/hackerthon5/avengers_BE/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.hackerthon5.avengers_BE.member.service;
 
 import com.hackerthon5.avengers_BE.member.DTO.SignupRequest;
+import com.hackerthon5.avengers_BE.member.domain.Member;
+import org.springframework.security.core.userdetails.User;
 
 
 public interface MemberService {
@@ -8,4 +10,5 @@ public interface MemberService {
     // 회원가입
     String signup(SignupRequest request);
 
+   Member myInfo(User user);
 }
