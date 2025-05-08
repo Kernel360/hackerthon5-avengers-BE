de70aa42fcfd472fe47db5ec19681ee18a1ab505
Soyun_p <ehouse190@gmail.com>
2025-05-08 20:47:21 +0900

    매핑문제

--- src/main/java/com/hackerthon5/avengers_BE/review/repository/ReviewRepository.java
+++ src/main/java/com/hackerthon5/avengers_BE/review/repository/ReviewRepository.java
@@ -5,14 +5,15 @@ import com.hackerthon5.avengers_BE.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findByMovieId(long movieId);
     List<Review> findByMemberId(long memberId);
-    @Query("SELECT r FROM Review r WHERE r.movieId IN :movieIds")
+    @Query("SELECT r FROM Review r WHERE r.movie.id IN :movieIds")
     List<Review> findAllByMovieIds(@Param("movieIds") List<Long> movieIds);
 }
