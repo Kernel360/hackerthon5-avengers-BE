c46baf6c8284f9109bac2c0535b4c796e665a365
SujeongJeong <suujeong0@gmail.com>
2025-05-08 15:05:31 +0900

    Remove .gradle from version control and resolve merge conflicts

--- src/main/java/com/hackerthon5/avengers_BE/member/domain/Member.java
+++ src/main/java/com/hackerthon5/avengers_BE/member/domain/Member.java
@@ -1,7 +1,5 @@
 package com.hackerthon5.avengers_BE.member.domain;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/hackerthon5/avengers_BE/member/service/MemberService.java
+++ src/main/java/com/hackerthon5/avengers_BE/member/service/MemberService.java
@@ -1,9 +1,6 @@
 package com.hackerthon5.avengers_BE.member.service;
 
-import com.hackerthon5.avengers_BE.member.DTO.LoginRequest;
 import com.hackerthon5.avengers_BE.member.DTO.SignupRequest;
-import com.hackerthon5.avengers_BE.member.DTO.TokenResponse;
-import com.hackerthon5.avengers_BE.member.domain.Member;
 
 
 public interface MemberService {
